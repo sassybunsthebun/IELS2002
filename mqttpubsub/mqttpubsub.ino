a623afa07af95e83ccfaf279327a4fc6992092f1
sassybunsthebun <sara.batevik@epost.no>
2024-10-19 09:07:24 +0200

    Cleaned up code and added comments

--- mqttpubsub/mqttpubsub.ino
+++ mqttpubsub/mqttpubsub.ino
@@ -22,7 +22,7 @@ char dataFromAVR[128];
 
 ///VARIABLES FOR SERIAL COMMUNICATION WITH AVR128DB48///
 
-//insert variables for serial communication with avr128db48 here//
+AVROutputPin = 21; 
 
 /**
 * @brief Initializes Wi-Fi connection and communcation with MQTT broker. Sets callback function for MQTT. 
@@ -32,11 +32,15 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  PinMode(AVROutputPin, OUTPUT);
 }
 
+/**
+* @brief Connects to WiFi. If WiFi is not connected, print dots. If connected, print IP address.  
+*/
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -53,7 +57,10 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
-
+/**
+* @brief The callback function that prints the message received from Raspberry Pi. 
+* Prints every char in the char array and stores it in the String messageTemp. 
+*/
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
@@ -67,17 +74,24 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.println();
 
   //If a message is recieved from raspi, send it to AVR128DB48
-  // If a message is received on the topic esp32/output, check if the message is either "on" or "off". 
+  //If a message is received on the topic esp32/output, check if the message is either "vibrate" or "off". 
 
   if (String(topic) == "esp32/output") {
     if(messageTemp == "vibrate"){
       Serial.println("vibrate");
+      DigitalWrite(AVROutputPin, HIGH);
+
     }
     else if(messageTemp == "off"){
       Serial.println("off");
+      DigitalWrite(AVROutputPin, LOW);
     }
   }
 }
+
+/**
+* @brief Connects to MQTT. If MQTT is not connected, print an error message.  
+*/
 
 void reconnect() {
   // Loop until we're reconnected
@@ -120,7 +134,8 @@ void loop() {
   }
   client.loop();
 
-  //Every 500 milliseconds, read the data from AVR128DB48 and add a zero at the end of the char array
+  //Every 500 milliseconds, read the data from AVR128DB48 and add a zero at the end of the char array 
+  //to signalize that the char array is over
 
   long now = millis();
   if (now - loopTimer > 500) {
